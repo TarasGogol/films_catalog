a3ab1e67443ffe66750ce217d67e2bc3782c0d99
Тарас Іванович <vip.kristals@gmail.com>
2022-12-26 10:31:42 +0200

    05-Loader

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
+//Налаштування
 const apiKey = "4fa85a04-56af-4127-ad30-1999f8a33e00"
-
 const url = "https://kinopoiskapiunofficial.tech/api/v2.2/films/";
 const options = {
     method: 'GET',
@@ -9,22 +9,42 @@ const options = {
     },
 }
 
-
-// fetch(url + 'top', options)
-//     .then(res => res.json())
-//     .then(json => console.log(json))
-//     .catch(err => console.log(err))
-
+//DOM елементи
 const filmsWrapper = document.querySelector(".films");
+const loader = document.querySelector(".loader-wrapper");
+const btnLoadMore = document.querySelector(".show-more");
 
+
+// Отримання і вивід топ 250 фільмів
 async function fetchAndRenderFilms(){
-	const response = await fetch(url + 'top', options);
-	const data = await response.json()
+	//Show preloader
+	loader.classList.remove('none')
+
+	//Fetch films data
+	const data = await fetchData(url + 'top', options);
 	console.log(data);
 
-	for(film of data.films){
-		console.log(film);
+	// Перевірка на додаткові сторінки і відображення кнопки
+	if(data.pagesCount > 1){
+		//Show button
+		btnLoadMore.classList.remove('none');
+	}
 
+	// Hide preloader
+	loader.classList.add('none')
+
+	//Render films on page
+	renderFilms(data.films);
+}
+
+async function fetchData(url, options){
+	const response = await fetch(url, options);
+	const data = await response.json()
+	return data;
+}
+
+function renderFilms(films){
+	for(film of films){
 		const html = `<div class="card">
 		<img src=${film.posterUrl} class="card__img">
 		<h3 class="card__title">${film.nameEn}</h3>
@@ -35,4 +55,5 @@ async function fetchAndRenderFilms(){
 	filmsWrapper.insertAdjacentHTML('beforeend', html);
 	}
 }
+
 fetchAndRenderFilms().catch((err) => console.log(err))
