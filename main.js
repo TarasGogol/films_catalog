097f9312194a36e5013778f43236f91de50c0a31
Тарас Іванович <vip.kristals@gmail.com>
2022-12-28 19:31:36 +0200

    06-display-dataMovies

--- main.js
+++ main.js
@@ -9,10 +9,13 @@ const options = {
     },
 }
 
+let page = 1;
+
 //DOM елементи
 const filmsWrapper = document.querySelector(".films");
 const loader = document.querySelector(".loader-wrapper");
-const btnLoadMore = document.querySelector(".show-more");
+const btnShowMore = document.querySelector(".show-more");
+btnShowMore.onclick = fetchAndRenderFilms;
 
 
 // Отримання і вивід топ 250 фільмів
@@ -21,13 +24,13 @@ async function fetchAndRenderFilms(){
 	loader.classList.remove('none')
 
 	//Fetch films data
-	const data = await fetchData(url + 'top', options);
-	console.log(data);
+	const data = await fetchData(url + `top?page=${page}`, options);
+	if (data.pagesCount > 1) page++
 
 	// Перевірка на додаткові сторінки і відображення кнопки
 	if(data.pagesCount > 1){
 		//Show button
-		btnLoadMore.classList.remove('none');
+		btnShowMore.classList.remove('none');
 	}
 
 	// Hide preloader
@@ -35,6 +38,9 @@ async function fetchAndRenderFilms(){
 
 	//Render films on page
 	renderFilms(data.films);
+
+	//Hide btn if show last pages
+	if(page > data.pagesCount) btnShowMore.classList.add('none');
 }
 
 async function fetchData(url, options){
@@ -45,15 +51,75 @@ async function fetchData(url, options){
 
 function renderFilms(films){
 	for(film of films){
-		const html = `<div class="card">
+		const card = document.createElement('div');
+		card.classList.add('card');
+		card.id = film.filmId;
+		
+		card.onclick = openFilmDetails;
+
+		const html = `
 		<img src=${film.posterUrl} class="card__img">
 		<h3 class="card__title">${film.nameEn}</h3>
 		<p class="card__year">${film.year}</p>
-		<p class="card__rate">rating ${film.rating}</p>
-	</div>`;
+		<p class="card__rate">rating ${film.rating}</p>`;
 
-	filmsWrapper.insertAdjacentHTML('beforeend', html);
+	card.insertAdjacentHTML('afterbegin', html)
+
+	filmsWrapper.insertAdjacentElement('beforeend', card);
 	}
+}
+
+async function openFilmDetails(e){
+	// Id movie
+	const id = e.currentTarget.id
+
+	// get data movies
+	const data = await fetchData(url + id, options)
+	console.log(data);
+
+	renderFilmsData(data);
+}
+
+function renderFilmsData(film){
+	console.log("render");
+
+
+	// 1. Render container-right
+	const containerRight = document.createElement('div')
+	containerRight.classList.add('container-right');
+	document.body.insertAdjacentElement('beforeend', containerRight)
+
+
+	// 2. btn close
+	const btnClose = document.createElement('button');
+	btnClose.classList.add('btn-close');
+	btnClose.innerHTML = '<img src="./img/cancel.svg" alt="">';
+	containerRight.insertAdjacentElement('afterbegin', btnClose);
+
+	// 2.1 btn close on click - delete container
+
+	btnClose.onclick = () => {
+		containerRight.remove();
+	}
+
+	// 3. Details movie
+	const html = `<div class="film">
+	<div class="film__title">${film.nameOriginal}</div>
+	<div class="film__img">
+		<img src="${film.posterUrl}" alt="${film.posterUrl}">
+	</div>
+	<div class="film__desk">
+		<p class="film__details">Year: ${film.year}</p>
+		<p class="film__details">Rate: ${film.ratingKinopoisk}</p>
+		<p class="film__details">Continuity: ${film.filmLength}</p>
+		<p class="film__details">Country: ${film.countries[0]['country']}</p>
+		<p class="film__text">${film.description}</p>
+		</div>
+	</div>`
+
+	containerRight.insertAdjacentHTML('beforeend',html)
+
+
 }
 
 fetchAndRenderFilms().catch((err) => console.log(err))
